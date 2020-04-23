e95c0e6d55c617470b6758f6b794773d8bd854dd
Hemant <hemantkumarh828@gmail.com>
2020-04-24 00:41:58 +0530

    The Github Card App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: [],
+    };
+  }
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profile: [...prevState.profile, profileData]
+    }));
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>The Github Card App</h1>
+        <Form onSubmit = {this.addNewProfile}/>
+        <CardList profile={this.state.profile}/>  
+      </div>
+    );
+  }
 }
 
+class Card extends Component {
+  render() {
+    const profile = this.props;
+    return(
+      <div className='github-profile'>
+        <img src={profile.avatar_url} />
+        <div className='info'>
+          <div className='Name'>{profile.name}</div>
+          <div className='Company'>{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+//card data in an array
+const CardList = (props) => (
+  <div>
+    {props.profile.map(profile => <Card key = {profile.id} {...profile}/>)}
+  </div>
+);
+
+/*input form
+fetching data from github api
+*/
+class Form extends Component {
+  state = {
+    userName: '',
+  };
+   handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+    this.props.onSubmit(resp.data);
+    this.setState({ userName: '' });
+  };
+  render() {
+    return(
+    <form onSubmit={this.handleSubmit}>
+        <input 
+        type='text'
+        value={this.state.userName} 
+        onChange = { event => this.setState({userName: event.target.value})}
+        placeholder='Github username' 
+        required />
+        <button>Add new card</button>
+      </form>
+    );
+  }
+}
 export default App;
